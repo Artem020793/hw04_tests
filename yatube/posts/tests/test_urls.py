6eadbb23959151ec834e5db451e94904a2b66662
Артем Римша <artem-rimsha@mail.ru>
2022-07-30 12:57:13 +0300

    Второй

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -45,13 +45,13 @@ class PostsURLTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create_url_authorized(self):
-        """Проверка доступа для авторизованного 
+        """Проверка доступа для авторизованного
         пользователя к созданию поста"""
         response = self.authorized_client.get('/create/')
         self.assertEqual(response.status_code, 200)
-        
+
     def test_create_url_unauthorized(self):
-        """Проверка доступа для неавторизованного 
+        """Проверка доступа для неавторизованного
         пользователя к созданию поста"""
         response = self.guest_client.get('/create/')
         self.assertEqual(response.status_code, 302)
@@ -61,33 +61,32 @@ class PostsURLTest(TestCase):
          к редактированию поста"""
         response = self.guest_client.get(f'/posts/{self.post.id}/edit/')
         self.assertEqual(response.status_code, 302)
-        
+
     def test_edit_url_not_by_author(self):
         """Проверка доступа для не автора к редактированию поста"""
         response = self.authorized_client_2.get(
             f'/posts/{self.post.id}/edit/')
         self.assertEqual(response.status_code, 302)
-        
+
     def test_unexisting_url(self):
         """Проверка несуществующей страницы"""
         response = self.guest_client.get('/unexisting/')
         self.assertEqual(response.status_code, 404)
-        
+
     def test_home_url_uses_correct_template(self):
         url_templates_names = {
-            reverse('posts:index') : 'posts/index.html',
-            reverse('posts:group_list', 
-                    kwargs={'slug': self.group.slug}) : 'posts/group_list.html',
-            reverse('posts:profile', 
-                    kwargs={'username': self.user.username}) : 'posts/profile.html',
-            reverse('posts:post_create') : 'posts/create_post.html',
-            reverse('posts:post_detail', 
-                    kwargs={'post_id': self.post.id}) : 'posts/post_detail.html',
-            reverse('posts:post_edit', 
-                    kwargs={'post_id': self.post.id}) : 'posts/create_post.html',
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:group_list',
+                    kwargs={'slug': self.group.slug}): 'posts/group_list.html',
+            reverse('posts:profile',
+                    kwargs={'username': self.user.username}): 'posts/profile.html',
+            reverse('posts:post_create'): 'posts/create_post.html',
+            reverse('posts:post_detail',
+                    kwargs={'post_id': self.post.id}): 'posts/post_detail.html',
+            reverse('posts:post_edit',
+                    kwargs={'post_id': self.post.id}): 'posts/create_post.html',
         }
         for address, template in url_templates_names.items():
             with self.subTest(address=address):
                 response = self.authorized_client.get(address)
                 self.assertTemplateUsed(response, template)
-                
